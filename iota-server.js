6c126dbae778a906a4f8df250e611ef7bf37c970
RUIZ VILLAFRANCA Valentin <valentin.ruiz@soprasteria.com>
2021-02-24 00:34:15 +0100

    commit

--- iota-server.js
+++ iota-server.js
@@ -1,9 +1,9 @@
 module.exports = function(RED) {
-    function IotaServerNode(n) {
+    function IotaServer(n) {
         RED.nodes.createNode(this,n);
         this.host = n.host;
         this.port = n.port;
         //host and port for iota provider
     }
-    RED.nodes.registerType("iota-server",IotaServerNode);
+    RED.nodes.registerType("iotaserver",IotaServer);
 }

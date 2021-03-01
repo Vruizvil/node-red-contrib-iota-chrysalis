0218e646fe9b5447530fc838e66fe07ec9421dff
RUIZ VILLAFRANCA Valentin <valentin.ruiz@soprasteria.com>
2021-03-01 02:08:47 +0100

    second commit

--- iota-messages.js
+++ iota-messages.js
@@ -44,60 +44,60 @@ module.exports = function(RED) {
             async function run_messageId(messageID) {
                   callback = await client.message(messageID) //.then(success,error);
                   msg.payload=callback;
-		  console.log("Done : ", callback);
+		              console.log("Done : ", callback);
+                  msg.payload.messageId = messageID;
                   msg.payload.payload.index = Buffer.from(callback.payload.index,'hex').toString('ascii');
                   msg.payload.payload.data = Buffer.from(callback.payload.data,'hex').toString('ascii');
                   self.send(msg);
                   //return callback;
             }
-	   function isEmpty(val){
-	        return (val === undefined || val == null || val.length <= 0) ? true : false;
-	   }
-	   function isMessageID(val) {
-	        return (val.length = 64 && iotajs.Converter.isHex(val)) ? true : false;
-	   }
-     function see_args(callback) {
-		     callback= msg.payload;
- 		      //console.log("init see_args: ", callback);
-          if (isEmpty(callback) || !isMessageID(callback)) {
-                callback = config.iotaValue;
-	              if (isEmpty(callback) || !isMessageID(callback)){
+	          function isEmpty(val){
+	                return (val === undefined || val == null || val.length <= 0) ? true : false;
+	          }
+	          function isMessageID(val) {
+	                return (val.length = 64 && iotajs.Converter.isHex(val)) ? true : false;
+	          }
+            function see_args(callback) {
+		            callback= msg.payload;
+ 		             //console.log("init see_args: ", callback);
+                if (isEmpty(callback) || !isMessageID(callback)) {
+                  callback = config.iotaValue;
+	                 if (isEmpty(callback) || !isMessageID(callback)){
 		                console.log("msg.payload incorrect messageID format: ", msg.payload);
 		                console.log("Args function incorrect messageID format: ", config.iotaValue);
 		                //callback = Buffer.from('0000000000000000000000000000000000000000000000000000000000000000','hex');
 		                callback = null;
-	              }
-          }
-          //console.log("end see_args: ", callback);
-          return callback;
-      }
-      function see_message(callback) {
-          messageData = config.iotaValue;
-          if (!isEmpty(msg.payload)) {
-            messageData = msg.payload;
-          }
-          let txt = JSON.stringify(messageData);
-          messageData = TRAN.transliterate(txt);
-          console.log("Done: ", messageData);
-          const submitMessage = {
+	                }
+                }
+                //console.log("end see_args: ", callback);
+                return callback;
+            }
+            function see_message(callback) {
+              messageData = config.iotaValue;
+              if (!isEmpty(msg.payload)) {
+                messageData = msg.payload;
+              }
+              let txt = JSON.stringify(messageData);
+              messageData = TRAN.transliterate(txt);
+              console.log("Done: ", messageData);
+              const submitMessage = {
               // Parents can be left undefined if you want the node to populate the field
               //parentMessageIds: client.tips().tipMessageIds.slice(0, iota_js_1.MAX_NUMBER_PARENTS),
               payload: {
                 type: iotajs.INDEXATION_PAYLOAD_TYPE,
                 index: iotajs.Converter.utf8ToHex("node-red-contrib-iota-Chrysalis"),
                 data: iotajs.Converter.utf8ToHex(messageData)
-              }
-          };
-          callback = submitMessage;
-          return callback;
-      }
+                }
+              };
+              callback = submitMessage;
+              return callback;
+            }
             if (this.readyIota) {
               console.log("Searching dataset...");
               this.readyIota = false;
               var self = this;
-              //this.status({fill:"red",shape:"ring",text:"connecting"});
-              //iota_value = config.iotaValue;
-
+              //var orig_status = this.status();
+              //this.status({fill:"blue",shape:"ring",text:"connecting"});
               switch (config.iotaSelect){
                 case 'messageDeserialize':
                   //const ArgsFunction = iota_value;
@@ -116,8 +116,6 @@ module.exports = function(RED) {
                   client.messagesFind(iotajs.Converter.utf8ToBytes(messageToFind)).then(success,error);
                   break;
                 case 'messageID':
-                  //console.log(see_args());
-                  //client.message(see_args()).then(success,error);
 		              messageID = see_args()
 		              if (!isEmpty(messageID)) {
 			                 run_messageId(messageID);
@@ -127,12 +125,11 @@ module.exports = function(RED) {
 			            }
                   break;
                 case 'messageSubmit':
-                  //iota_value = msg.payload;
                   submitMessage = see_message();
                   client.messageSubmit(submitMessage).then(success,error);
                   break;
                 }
-                //this.status({});
+                //this.status(orig_status);
 		            this.readyIota = true;
             }
         });

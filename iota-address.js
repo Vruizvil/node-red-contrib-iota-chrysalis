bee97de873f6c157bff8bbe6f44583e6fb8df413
RUIZ VILLAFRANCA Valentin <valentin.ruiz@soprasteria.com>
2021-03-24 22:50:43 +0100

    commit v1.2.0

--- iota-address.js
+++ iota-address.js
@@ -48,46 +48,11 @@ module.exports = function(RED) {
               self.send(msg);
               //return callback;
             }
-            async function run_messageId(messageID) {
-                  callback = await client.message(messageID) //.then(success,error);
-                  msg.payload=callback;
-		              console.log("Done : ", callback);
-                  msg.payload.messageId = messageID;
-                  msg.payload.payload.index = Buffer.from(callback.payload.index,'hex').toString('ascii');
-                  msg.payload.payload.data = Buffer.from(callback.payload.data,'hex').toString('ascii');
-                  self.send(msg);
-                  //return callback;
-            }
-            async function bech32ToHex(val) {
-              //callback = iotajs.Converter.bytesToHex(iotajs.Bech32Helper.fromBech32(val, node.bech32HRP).addressBytes)
-              //console.log("bech32ToHex: ", val, callback);
-              if (iotajs.Bech32Helper.matches(val, node.bech32HRP)) {
-                ad = await client.address(val);
-                callback = ad.address;
-                console.log("Bech32 to Hex: ", val, callback);
-              } else {
-                  ad = await client.addressEd25519(val);
-                  ad_bech = iotajs.Bech32Helper.toBech32(iotajs.ED25519_ADDRESS_TYPE, iotajs.Converter.hexToBytes(ad.address), node.bech32HRP);
-                  console.log("Hex to Bech32: ", ad.address, ad_bech);
-                  callback = val;
-                };
-              return callback;  //return Hex format
-            }
+
 	          function isEmpty(val){
 	                return (val === undefined || val == null || val.length <= 0) ? true : false;
 	          }
-	          function isMessageID(val) {
-                  //console.log("isMessageID length isHex?: ", val, val.length, iotajs.Converter.isHex(val));
-	                return (val.length = 64 && iotajs.Converter.isHex(val)) ? true : false;
-	          }
-            function isAddress(val) {
-                  add_hex = bech32ToHex(val);
-                  return isMessageID(add_hex);
-                  //return (bech32ToHex(val).length = 64 && iotajs.Converter.isHex(bech32ToHex(val))) ? true : false;
-            }
-            function isOutput(val) {
-                  return (val.length = 68 && iotajs.Converter.isHex(val)) ? true : false;
-            }
+
             async function run_addr(callback) {
                     addr = callback
                     if (!iotajs.Bech32Helper.matches(addr, node.bech32HRP)) {
@@ -97,8 +62,9 @@ module.exports = function(RED) {
                          ad = await client.address(addr);
                          bech_ad = addr;
                          };
-                  console.log("Origen ADDR Hex Bech32: ", ad.address, bech_ad);
-                  return bech_ad;
+                  console.log("Address Hex Bech32: ", ad.address, bech_ad);
+                  msg.addressBech32 = bech_ad;
+                  //return bech_ad;
                  }
             function see_args(callback) {
 		            callback= msg.payload;
@@ -113,26 +79,7 @@ module.exports = function(RED) {
 	                }
                 return callback;
             }
-            function see_message(callback) {
-              messageData = config.iotaMessage;
-              if (!isEmpty(msg.payload)) {
-                messageData = msg.payload;
-              }
-              let txt = JSON.stringify(messageData);
-              messageData = TRAN.transliterate(txt);
-              console.log("Done: ", messageData);
-              const submitMessage = {
-              // Parents can be left undefined if you want the node to populate the field
-              //parentMessageIds: client.tips().tipMessageIds.slice(0, iota_js_1.MAX_NUMBER_PARENTS),
-              payload: {
-                type: iotajs.INDEXATION_PAYLOAD_TYPE,
-                index: iotajs.Converter.utf8ToHex("node-red-contrib-iota-Chrysalis"),
-                data: iotajs.Converter.utf8ToHex(messageData)
-                }
-              };
-              callback = submitMessage;
-              return callback;
-            }
+
             if (this.readyIota) {
               console.log("Running iota-transfers...");
               this.readyIota = false;
@@ -141,40 +88,38 @@ module.exports = function(RED) {
                 case 'AddressInfo':
                   addr_from = see_args();
                   if (!isEmpty(addr_from)) {
-                     bech32Addr = run_addr(addr_from);
-                     msg.addressFrom = bech32Addr;
+                     run_addr(addr_from);
                    } else {
-                     msg.addressFrom = "Error: Incorrect Address format";
+                     msg.addressBech32 = "Error: Incorrect Address format";
                      break;
                      //self.send(msg);
                    }
-                  client.address(bech32Addr).then(success,error);
+                  client.address(msg.addressBech32).then(success,error);
                   break;
                 case 'AddressOutput':
                   addr_from = see_args();
                   if (!isEmpty(addr_from)) {
-                     bech32Addr = run_addr(addr_from);
-                     msg.addressFrom = bech32Addr;
+                     run_addr(addr_from);
                    } else {
-                     msg.addressFrom = "Error: Incorrect Address format";
+                     msg.addressBech32 = "Error: Incorrect Address format";
                      break;
                      //self.send(msg);
                    }
-                  client.addressOutputs(bech32Addr).then(success,error);
+                  client.addressOutputs(msg.addressBech32).then(success,error);
                   break;
                 case 'HexToBech32':
                   addr_from = see_args();
                   if (!isEmpty(addr_from)) {
-                     bech32Addr = run_addr(addr_from);
-                     msg.payload = bech32Addr;
+                     run_addr(addr_from);
+                     msg.payload = msg.addressBech32;
                      self.send(msg);
                   }
                 break;
                 case 'Bech32ToHex':
                   addr_from = see_args();
                   if (!isEmpty(addr_from)) {
-                    bech32Addr = run_addr(addr_from);
-                    msg.payload = iotajs.Converter.bytesToHex(iotajs.Bech32Helper.fromBech32(bech32Addr, node.bech32HRP).addressBytes);
+                    run_addr(addr_from);
+                    msg.payload = iotajs.Converter.bytesToHex(iotajs.Bech32Helper.fromBech32(msg.addressBech32, node.bech32HRP).addressBytes);
                     self.send(msg);
                   }
                 break;

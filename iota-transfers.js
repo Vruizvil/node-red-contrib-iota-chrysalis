073d6440e07117fabfb886df0bda37100d20a38e
RUIZ VILLAFRANCA Valentin <valentin.ruiz@soprasteria.com>
2021-04-08 22:51:02 +0200

    adjust console.log messages

--- iota-transfers.js
+++ iota-transfers.js
@@ -120,6 +120,7 @@ module.exports = function(RED) {
                      key: messageKey.toString(),
                      data: messageData.toString()
                    };
+                   console.log("Submit Message : ", typeof(submitMessage), submitMessage);
 
                    await iotajs.send(client, walletSeed, 0, bech_ad, amount, submitMessage).then(success,error);
                    //await iotajs.sendMultiple(client, walletSeed,0, jsonOutputs, submitMessage).then(success,error);
